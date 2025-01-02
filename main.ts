beea0acce561bbdb8c5b11bcdc70167bccb19459
Janus <janus@insignificancegalore.net>
2025-01-02 22:10:56 +0100

    add speed

--- main.ts
+++ main.ts
@@ -1,14 +1,21 @@
+let speed = 0
 let tilt = 0
 led.plot(0, 0)
 let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
 strip.setBrightness(63)
 strip.showRainbow(1, 360)
+loops.everyInterval(50, function () {
+    if (tilt != 0) {
+        strip.rotate(tilt * speed)
+        strip.show()
+    }
+})
 basic.forever(function () {
     if (input.acceleration(Dimension.X) < -100) {
         basic.showLeds(`
             . . # . .
             . # . . .
-            # # # # #
+            # . . . #
             . # . . .
             . . # . .
             `)
@@ -17,7 +24,7 @@ basic.forever(function () {
         basic.showLeds(`
             . . # . .
             . . . # .
-            # # # # #
+            # . . . #
             . . . # .
             . . # . .
             `)
@@ -32,10 +39,13 @@ basic.forever(function () {
             `)
         tilt = 0
     }
-})
-loops.everyInterval(100, function () {
-    if (tilt != 0) {
-        strip.rotate(tilt)
-        strip.show()
+    speed = Math.round(Math.sqrt(Math.constrain(Math.abs(input.acceleration(Dimension.X)), 100, 900) / 100))
+    if (speed == 2) {
+        led.plot(1, 2)
+        led.plot(3, 2)
+    } else if (speed == 1) {
+        led.plot(1, 2)
+        led.plot(2, 2)
+        led.plot(3, 2)
     }
 })
